7e6d11d9db8f6b9c29aed57db57f969af5158054
ChickenBenny <zxc123benny14159@gmail.com>
2022-11-01 16:04:04 +0800

    feat: init team table script

--- database/sql/create_tables.sql
+++ database/sql/create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS team (
     team_id SERIAL PRIMARY KEY,
-    team_name VARCHAR(20) UNIQUE,
-    stadium VARCHAR(20)
+    team_name VARCHAR(25) UNIQUE,
+    stadium VARCHAR(40)
 );
 
 CREATE TABLE IF NOT EXISTS player (
@@ -54,3 +54,35 @@ CREATE TABLE IF NOT EXISTS game (
         FOREIGN KEY(ref_id)
             REFERENCES referee(ref_id)     
 );
+
+-- init team table
+INSERT INTO team(team_id,team_name,stadium) VALUES('1','Chicago White Sox','Guaranteed Rate Field');
+INSERT INTO team(team_id,team_name,stadium) VALUES('2','Cleveland Guardians','Progressive Field');
+INSERT INTO team(team_id,team_name,stadium) VALUES('3','Detroit Tigers','Comerica Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('4','Kansas City Royals','Kauffman Stadium');
+INSERT INTO team(team_id,team_name,stadium) VALUES('5','Minnesota Twins','Target Field');
+INSERT INTO team(team_id,team_name,stadium) VALUES('6','Chicago Cubs','Wrigley Field');
+INSERT INTO team(team_id,team_name,stadium) VALUES('7','Cincinnati Reds','Great American Ball Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('8','Milwaukee Brewers','American Family Fielddouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('9','Pittsburgh Pirates','PNC Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('10','St. Louis Cardinals','Busch Stadium');
+INSERT INTO team(team_id,team_name,stadium) VALUES('11','Baltimore Orioles','Oriole Park at Camden Yards');
+INSERT INTO team(team_id,team_name,stadium) VALUES('12','Boston Red Sox','Fenway Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('13','New York Yankees','Yankee Stadium');
+INSERT INTO team(team_id,team_name,stadium) VALUES('14','Tampa Bay Rays','Tropicana Fielddagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('15','Toronto Blue Jays','Rogers Centredouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('16','Atlanta Braves','Truist Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('17','New York Mets','LoanDepot Parkdouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('18','Philadelphia Phillies','Citi Field');
+INSERT INTO team(team_id,team_name,stadium) VALUES('19','Washington Nationals','Citizens Bank Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('20','Los Angeles Angels','Nationals Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('21','Oakland Athletics','Angel Stadium');
+INSERT INTO team(team_id,team_name,stadium) VALUES('22','Seattle Mariners','T-Mobile Parkdouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('23','Texas Rangers','Globe Life Fielddouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('24','Arizona Diamondbacks','Chase Fielddouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('25','Colorado Rockies','Coors Field');
+INSERT INTO team(team_id,team_name,stadium) VALUES('26','Los Angeles Dodgers','Dodger Stadium');
+INSERT INTO team(team_id,team_name,stadium) VALUES('27','San Diego Padres','Petco Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('28','San Francisco Giants','Oracle Park');
+INSERT INTO team(team_id,team_name,stadium) VALUES('29','Houston Astros','Minute Maid Parkdouble-dagger');
+INSERT INTO team(team_id,team_name,stadium) VALUES('30','Miami Marlins','LoanDepot Parkdouble-dagger');
